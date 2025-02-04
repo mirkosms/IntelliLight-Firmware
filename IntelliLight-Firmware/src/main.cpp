20328fe54a852aaea568a168591fc07a52aa2d42
mirkosms <miroslawsmoronski+git@gmail.com>
2025-02-04 23:56:23 +0100

    Implemented basic LED control with FastLED for WS2812B strip

--- IntelliLight-Firmware/src/main.cpp
+++ IntelliLight-Firmware/src/main.cpp
@@ -1,11 +1,21 @@
-#include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_PIN 5       // Pin ESP32 podłączony do Data In taśmy LED
+#define NUM_LEDS 30     // Liczba diod na taśmie
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Hello from IntelliLight Firmware!");
+    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void loop() {
-    Serial.println("Running...");
-    delay(1000);
+    // Wszystkie diody świecą na biało (maksymalna jasność 255)
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB(255, 255, 255);
+    }
+    FastLED.show();
+    delay(1000);  // Odświeżanie co 1s
 }
